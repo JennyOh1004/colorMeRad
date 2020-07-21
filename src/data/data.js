faaa31f76c1b86e657d2021bfd01ec08c2ca3b96
JennyOh1004 <ohseulki1004@gmail.com>
2020-07-20 21:27:46 -0700

    Add project data

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const projectData = [
+  {
+    title: "Welcome!",
+    description:
+      "We believe every woman is beautiful in her own way. We’re here to help you to find your best beauty and style.",
+  },
+  {
+    title: "DISCOVER YOUR COLORS",
+    description:
+      "Start off a quick color analysis to find your color type. With just one selfie, you’ll get which colors go best with you",
+  },
+  {
+    title: "SHOP YOUR BEST ITEMS",
+    description:
+      "After the analysis, browse hundreds of product recommendations that best fit in your color type",
+  },
+  {
+    title: "EXPLORE MORE STYLES",
+    description:
+      "After the analysis, browse hundreds of product recommendations that best fit in your color type",
+  },
+];
